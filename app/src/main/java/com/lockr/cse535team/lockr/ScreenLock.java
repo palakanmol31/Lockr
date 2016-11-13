cea46522523dfb92c510a0a74b87834189fe9c6a
Rashik Kotwal (Student) <rkotwal2@sundevils.asu.edu>
2016-11-13 14:16:22 -0700

    saving work

--- app/src/main/java/com/lockr/cse535team/lockr/ScreenLock.java
+++ app/src/main/java/com/lockr/cse535team/lockr/ScreenLock.java
@@ -1,17 +1,41 @@
 package com.lockr.cse535team.lockr;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.takwolf.android.lock9.Lock9View;
 
 /**
  * Created by rkotwal2 on 11/13/2016.
  */
 
 public class ScreenLock extends Activity {
+    SharedPreferences sharedPreferences;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popup_unlock);
+        Lock9View lock9View = (Lock9View) findViewById(R.id.lock_9_view);
+
+        lock9View.setCallBack(new Lock9View.CallBack() {
+            @Override
+            public void onFinish(String password) {
+
+                if (sharedPreferences.getString(AppLockConstants.PASSWORD, "").matches(password)) {
+                    Toast.makeText(getApplicationContext(), "Success : Password Match", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(ScreenLock.this, LoadingActivity.class);
+                    startActivity(i);
+                    finish();
+                    //AppLockLogEvents.logEvents(AppLockConstants.PASSWORD_CHECK_SCREEN, "Correct Password", "correct_password", "");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Pattern Try Again", Toast.LENGTH_SHORT).show();
+                    //AppLockLogEvents.logEvents(AppLockConstants.PASSWORD_CHECK_SCREEN, "Wrong Password", "wrong_password", "");
+                }
+            }
+        });
     }
 
 }
